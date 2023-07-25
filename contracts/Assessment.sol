4146a32c3054617f61a2382df6791eeb79c9ebf9
Ankit Jangra <135188111+ankitjangra114@users.noreply.github.com>
2023-07-25 08:43:54 +0530

    Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,65 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
-
-//import "hardhat/console.sol";
-
-contract Assessment {
-   
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-    
-
+contract ABC {
+    event Depo(uint256 amount);
+    event draw(uint256 amount);
     mapping(address => uint256) public balanceOf;
     mapping(address => address) public ownerOf;                                                                                                                                         
-      
-    function getBalanceFromWalletAddress(address walletAddress) public view returns(uint256) {
-    return balanceOf[walletAddress];
+    function Balance(address wallAddress) public view returns(uint256) {
+    return balanceOf[wallAddress];
     }
-    
-
-    function depositamount(uint256 _amount) public payable {
-        // Get the previous balance of the account
-        uint _previousbalance= balanceOf[msg.sender];
-
-
-        // perform transaction
+    function depoamount(uint256 _amount) public payable {
+        uint prebalance= balanceOf[msg.sender];
         balanceOf[msg.sender] += _amount;
-
-
-        // assert transaction completed successfully
-        assert(balanceOf[msg.sender] == _previousbalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        assert(balanceOf[msg.sender] == prebalance + _amount);
+        emit Depo(_amount);
     }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdrawamount(uint256 _withdrawAmount) public {
-       
-        uint _previousBalance = balanceOf[msg.sender];
-        if (balanceOf[msg.sender] < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: _previousBalance,
-                withdrawAmount: _withdrawAmount
+    error Insufficient(uint256 balance, uint256 drawAmount);
+    function drawamount(uint256 _drawAmount) public {
+        uint prebalance = balanceOf[msg.sender];
+        if (balanceOf[msg.sender] < _drawAmount) {
+            revert Insufficient({
+                balance: prebalance,
+                drawAmount: _drawAmount
             });
         }
-
-        // withdrawamount
-        balanceOf[msg.sender] -= _withdrawAmount;
-
-        // assert if the balance is correct
-        assert(balanceOf[msg.sender] == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        balanceOf[msg.sender] -= _drawAmount;
+        assert(balanceOf[msg.sender] == (prebalance - _drawAmount));
+        emit draw(_drawAmount);
     }
-    
-    // Function to check the balance of an account
-    function checkBalance() public view returns(uint256) {
+    function check() public view returns(uint256) {
     address account = msg.sender;
     return balanceOf[account];
     }
- 
 }
